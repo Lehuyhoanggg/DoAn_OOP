165be0d789370205e6e2a9332c0cdcd22e97234a
coderrrbao <5hoaibao51265126@gmail.com>
2025-09-21 16:38:22 +0700

    thay doi interface LamViec

--- interfaces/LamViec.java
+++ interfaces/LamViec.java
@@ -1,14 +1,25 @@
 package interfaces;
 
 public interface LamViec {
+    // hien thi thong tin
+
     public void diemDanhCaLam();
 
     public void xemLichLamViec();
 
-    public void quanLyDonHang();
+    public void traCuuThongTinKhachHang();
 
-    public void quanLyBaoHanh();
+    public void taoHoaDon();
 
-    public void quanLyKhacHang();
+    public void taoPhieuTraHang();
 
+    public void timKiemSanPham();
+
+    public void TimKiemBaoHanh();
+
+    public void taoBaoHanh();
+
+    public void xemTatCaMaGiamGia();
+
+    // doi mat khau
 }
