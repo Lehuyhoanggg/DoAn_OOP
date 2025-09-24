cfc3b67235a6ee3200ae27434890d306aebfc00a
PhamKhanh08 <phukhanh1499@gmail.com>
2025-09-24 07:26:32 +0700

    Update SanPhamService.java

--- service/SanPhamService.java
+++ service/SanPhamService.java
@@ -1,30 +1,46 @@
 package service;
-
 import java.util.ArrayList;
-
 import model.SanPham;
 
 public class SanPhamService {
-    ArrayList<SanPham> listSanPham;
+    private ArrayList<SanPham> listSanPham;
+
+    public SanPhamService() {
+        listSanPham = new ArrayList<>();
+    }
 
     public SanPhamService(ArrayList<SanPham> listSanPham) {
         this.listSanPham = listSanPham;
     }
 
-    public void themSanPham(SanPham sanPham) {
-
+    public ArrayList<SanPham> getListSanPham() {
+        return listSanPham;
     }
 
-    public SanPham timSanPham(String ma) {
-        if (listSanPham == null) {
-            return null;
-        }
+    public void setListSanPham(ArrayList<SanPham> listSanPham) {
+        this.listSanPham = listSanPham;
+    }
+
+    public void themSanPham(SanPham sanPham) {
+        listSanPham.add(sanPham);
+    }
+
+    public void xoaSanPham(String ma) {
+        if (listSanPham == null) return;
+        listSanPham.removeIf(sp -> sp.getMa().equals(ma));
+    }
+
+    public void suaSanPham(SanPham sanPham) {
+        if (listSanPham == null) return;
         for (int i = 0; i < listSanPham.size(); i++) {
-            if (listSanPham.get(i).getMa().equals(ma)) {
-                return listSanPham.get(i);
+            if (listSanPham.get(i).getMa().equals(sanPham.getMa())) {
+                listSanPham.set(i, sanPham);
+                break;
             }
         }
-        return null;
     }
-
+    public void searchSanPham(String ten) {
+        if (listSanPham == null) return;
+        listSanPham.removeIf(sp -> !sp.getTen().toLowerCase().contains(ten.toLowerCase()));
+    }
 }
